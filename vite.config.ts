d077f622298d58baf356a3cd7dc0b953a89b7697
damianof <damianof@users.noreply.github.com>
2025-12-29 18:10:30 -0500

    Syncing version numbe rs across packages

--- vite.config.ts
+++ vite.config.ts
@@ -57,16 +57,37 @@ export default defineConfig({
     },
     rollupOptions: {
       // THIS IS VITAL: The SDK must not contain Vue code
-      external: ['vue'],
+      external: ['vue', '@tenorlab/dashboard-core'],
       output: {
-        // This ensures your main file is named vue-dashboard.es.js
-        // and core is named core.es.js
+        // // This ensures your main file is named vue-dashboard.es.js
+        // // and core is named core.es.js
+        // entryFileNames: (chunkInfo) => {
+        //   // Matches the key in your lib.entry object
+        //   return chunkInfo.name === 'vue-dashboard' ? 'vue-dashboard.es.js' : '[name].es.js'
+        // },
+        // // If shared code is split out, name it something recognizable
+        // chunkFileNames: 'chunks/[name].js',
+        // assetFileNames: (assetInfo) => {
+        //   const name = assetInfo.names?.[0] ?? assetInfo.name
+        //   if (name && name.endsWith('.css')) {
+        //     return 'styles.css'
+        //   }
+        //   return '[name][extname]'
+        // },
+
+        // Ensure NO chunks are created.
+        // This forces everything into the entry files.
+        manualChunks: undefined,
+        preserveModules: false,
         entryFileNames: (chunkInfo) => {
-          // Matches the key in your lib.entry object
           return chunkInfo.name === 'vue-dashboard' ? 'vue-dashboard.es.js' : '[name].es.js'
         },
-        // If shared code is split out, name it something recognizable
-        chunkFileNames: 'chunks/[name].js',
+        // 3. THE MAGIC BIT:
+        // This tells Rollup: "When you see an import for @tenorlab/dashboard-core,
+        // rewrite the path to @tenorlab/vue-dashboard/core in the final JS."
+        paths: {
+          '@tenorlab/dashboard-core': '@tenorlab/vue-dashboard/core',
+        },
         assetFileNames: (assetInfo) => {
           const name = assetInfo.names?.[0] ?? assetInfo.name
           if (name && name.endsWith('.css')) {
